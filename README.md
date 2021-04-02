46560d4414bee49512f38b0813a1a3f6eb9ba770
ehb54 <brookes@uthscsa.edu>
2021-04-02 12:54:16 +0000

    added table_record_counts.php

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ## Various utilities for managing the database
 
- - php field_dep_tree.php
-   - produces a dependency graph of foreign key constraints
-   - general tool for this, can be used with any mysql database
-   
+ - general tools, can be used with any mysql database
+   - php field_dep_tree.php
+     - produces a dependency graph of foreign key constraints
+     - general tool for this, can be used with any mysql database
+   - php table_record_counts.php
+     - returns a list of record counts
+     
+
+
---
+++ table_record_counts.php
@@ -0,0 +1,107 @@
+<?php
+
+# developer defines
+$logging_level = 2;
+# end of developer defines
+
+$self = __FILE__;
+    
+$notes = <<<__EOD
+usage: $self dbname {remote_config_file}
+
+list record counts of all tables in db
+if remote_config_file specified, it will be used instead of db config
+__EOD;
+
+if ( count( $argv ) < 2 || count( $argv ) > 3 ) {
+    echo $notes;
+    exit;
+}
+
+$lims_db = $argv[ 1 ];
+
+$config_file = "db_config.php";
+if ( count( $argv ) == 3 ) {
+    $use_config_file = $argv[ 2 ];
+} else {
+    $use_config_file = $config_file;
+}
+
+if ( !file_exists( $use_config_file ) ) {
+    fwrite( STDERR, "$self: 
+$use_config_file does not exist
+
+to fix:
+
+cp ${config_file}.template $use_config_file
+and edit with appropriate values
+")
+        ;
+    exit(-1);
+}
+            
+require $use_config_file;
+
+
+# utility
+
+function write_logl( $msg, $this_level = 0 ) {
+    global $logging_level;
+    global $self;
+    if ( $logging_level >= $this_level ) {
+        echo "${self}: " . $msg . "\n";
+    }
+}
+
+function db_obj_result( $db_handle, $query, $expectedMultiResult = false ) {
+    $result = mysqli_query( $db_handle, $query );
+
+    if ( !$result || !$result->num_rows ) {
+        if ( $result ) {
+            # $result->free_result();
+        }
+        write_logl( "db query failed : $query\ndb query error: " . mysqli_error($db_handle) . "\n" );
+        if ( $result ) {
+            debug_json( "query result", $result );
+        }
+        exit;
+    }
+
+    if ( $result->num_rows > 1 && !$expectedMultiResult ) {
+        write_logl( "WARNING: db query returned " . $result->num_rows . " rows : $query" );
+    }    
+
+    if ( $expectedMultiResult ) {
+        return $result;
+    } else {
+        return mysqli_fetch_object( $result );
+    }
+}
+
+function debug_json( $msg, $json ) {
+    fwrite( STDERR,  "$msg\n" );
+    fwrite( STDERR, json_encode( $json, JSON_PRETTY_PRINT ) );
+    fwrite( STDERR, "\n" );
+}
+
+# main
+
+$db_handle = mysqli_connect( $dbhost, $user, $passwd, $lims_db );
+if ( !$db_handle ) {
+    write_logl( "could not connect to mysql: $dbhost, $user, $lims_db. exiting\n" );
+    exit(-1);
+}
+
+$res = db_obj_result( $db_handle, "show tables from $lims_db", true );
+$tables = [];
+while( $row = mysqli_fetch_array($res) ) {
+    $tables[] = $row[ "Tables_in_$lims_db" ];
+}
+debug_json( "tables", $tables);
+
+foreach ( $tables as $k => $v ) {
+    $q = "select count(*) $v";
+    $res = db_obj_result( $db_handle, $q );
+    $res = $res->{$v};
+    echo "$v $res\n";
+}
