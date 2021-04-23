ce8155e66b6283ac8fc613d213a565d8cd28c2b3
ehb54 <brookes@uthscsa.edu>
2021-04-23 08:59:05 -0600

    moved utility routines into utility.php, added uslims_db_variables.php, renamed uslims3_dbs.php to uslims_dbs.php

--- table_record_counts.php
+++ table_record_counts.php
@@ -42,48 +42,7 @@ and edit with appropriate values
 }
             
 require $use_config_file;
-
-
-# utility
-
-function write_logl( $msg, $this_level = 0 ) {
-    global $logging_level;
-    global $self;
-    if ( $logging_level >= $this_level ) {
-        echo "${self}: " . $msg . "\n";
-    }
-}
-
-function db_obj_result( $db_handle, $query, $expectedMultiResult = false ) {
-    $result = mysqli_query( $db_handle, $query );
-
-    if ( !$result || !$result->num_rows ) {
-        if ( $result ) {
-            # $result->free_result();
-        }
-        write_logl( "db query failed : $query\ndb query error: " . mysqli_error($db_handle) . "\n" );
-        if ( $result ) {
-            debug_json( "query result", $result );
-        }
-        exit;
-    }
-
-    if ( $result->num_rows > 1 && !$expectedMultiResult ) {
-        write_logl( "WARNING: db query returned " . $result->num_rows . " rows : $query" );
-    }    
-
-    if ( $expectedMultiResult ) {
-        return $result;
-    } else {
-        return mysqli_fetch_object( $result );
-    }
-}
-
-function debug_json( $msg, $json ) {
-    fwrite( STDERR,  "$msg\n" );
-    fwrite( STDERR, json_encode( $json, JSON_PRETTY_PRINT ) );
-    fwrite( STDERR, "\n" );
-}
+require "utility.php";
 
 # main
 
---
+++ uslims_db_variables.php
@@ -0,0 +1,85 @@
+<?php
+
+# developer defines
+$logging_level = 2;
+
+$variables_of_interest = [
+    "symbolic-links"
+    ,"skip-external-locking"
+    ,"key_buffer_size"
+    ,"max_allowed_packet"
+    ,"table_open_cache"
+    ,"table_definition_cache"
+    ,"sort_buffer_size"
+    ,"net_buffer_length"
+    ,"read_buffer_size"
+    ,"read_rnd_buffer_size"
+    ,"join_buffer_size"
+    ,"myisam_sort_buffer_size"
+    ,"connect_timeout"
+    ,"expire_logs_days"
+    ,"innodb_file_per_table"
+    ,"max_connections"
+    ,"max_user_connections"
+    ,"thread_cache_size"
+    ,"query_cache_type"
+    ,"query_cache_size"
+    ];
+# end of developer defines
+
+$self = __FILE__;
+    
+$notes = <<<__EOD
+usage: $self {remote_config_file}
+
+lists mysqld variables of interest for uslims
+if remote_config_file specified, it will be used instead of db config
+
+__EOD;
+
+if ( count( $argv ) < 1 || count( $argv ) > 2 ) {
+    echo $notes;
+    exit;
+}
+
+$config_file = "db_config.php";
+if ( count( $argv ) == 2 ) {
+    $use_config_file = $argv[ 1 ];
+} else {
+    $use_config_file = $config_file;
+}
+
+if ( !file_exists( $use_config_file ) ) {
+    fwrite( STDERR, "$self: 
+$use_config_file does not exist
+
+to fix:
+
+cp ${config_file}.template $use_config_file
+and edit with appropriate values
+")
+        ;
+    exit(-1);
+}
+            
+require $use_config_file;
+require "utility.php";
+
+# main
+
+$db_handle = mysqli_connect( $dbhost, $user, $passwd );
+if ( !$db_handle ) {
+    write_logl( "could not connect to mysql: $dbhost, $user, $lims_db. exiting\n" );
+    exit(-1);
+}
+
+$res = db_obj_result( $db_handle, "show global variables", true );
+$variables = [];
+while( $row = mysqli_fetch_array($res) ) {
+    $var = $row[0];
+    $val = $row[1];
+    if ( in_array( $var, $variables_of_interest ) ) {
+        echo "$var $val\n";
+    }
+}
+
