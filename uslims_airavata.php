5fcbcf0d7bf05848e9cf906f354905a47d7ec19e
ehb54 <brookes@uthscsa.edu>
2023-04-05 15:51:39 +0000

    uslims_airavata.php - add compute resource info

--- uslims_airavata.php
+++ uslims_airavata.php
@@ -9,6 +9,7 @@ $us3bin       = "$us3lims/bin";
 include "$us3bin/listen-config.php";
 include $class_dir_p . "experiment_cancel.php";
 include $class_dir_p . "experiment_status.php";
+include $class_dir_p . "experiment_resource.php";
 
 $self = __FILE__;
     
@@ -22,7 +23,9 @@ Options
 --help                     : print this information and exit
 
 --gfacid             id    : set gfacid
+--all                      : list status for all Airavata jobs in gfac.analysis
 --cancel                   : cancel job
+
 
 __EOD;
 
@@ -32,6 +35,7 @@ array_shift( $u_argv ); # first element is program name
 
 $gfacid    = false;
 $cancel    = false;
+$all       = false;
 
 while( count( $u_argv ) && substr( $u_argv[ 0 ], 0, 1 ) == "-" ) {
     $anyargs = true;
@@ -53,18 +57,77 @@ while( count( $u_argv ) && substr( $u_argv[ 0 ], 0, 1 ) == "-" ) {
             $cancel = true;
             break;
         }
+        case "--all": {
+            array_shift( $u_argv );
+            $all = true;
+            break;
+        }
       default:
         error_exit( "\nUnknown option '$u_argv[0]'\n\n$notes" );
     }        
+}
+
+$config_file = "db_config.php";
+if ( count( $u_argv ) ) {
+    $use_config_file = array_shift( $u_argv );
+} else {
+    $use_config_file = $config_file;
+}
+if ( !$anyargs || count( $u_argv ) ) {
+    echo $notes;
+    exit;
+}
+
+if ( !file_exists( $use_config_file ) ) {
+    fwrite( STDERR, "$self: 
+$use_config_file does not exist
+
+to fix:
+
+cp ${config_file}.template $use_config_file
+and edit with appropriate values
+")
+        ;
+    exit(-1);
+}
+
+file_perms_must_be( $use_config_file );
+require $use_config_file;
+
+function gfacstatus( $id ) {
+    echoline();
+    echo "status for $id:\n" . json_encode( getExperimentStatus( $id ), JSON_PRETTY_PRINT ) . "\n";
+    echo "compute resource for $id:\n" . json_encode( getComputeResource( $id ), JSON_PRETTY_PRINT ) . "\n";
+    echoline();
+}
+    
+
+if ( $all ) {
+    open_db();
+    $res = db_obj_result( $db_handle, "select gfacID from gfac.analysis", True, True );
+    $gfacids = [];
+    if ( $res ) {
+        while( $row = mysqli_fetch_array($res) ) {
+            # debug_json( "row ", $row );
+            if ( preg_match( '/^US3-AIRA/', $row['gfacID'] ) ) {
+                $gfacids[] = $row['gfacID'];
+            }
+        }
+    }
+    
+    # debug_json( "gfacids", $gfacids );
+    foreach ( $gfacids as $v ) {
+        gfacstatus( $v );
+    }
+        
+    exit;
 }
 
 if ( !$gfacid ) {
     error_exit( "--gfacid must be specified" );
 }
 
-echoline();
-echo "status for $gfacid:\n" . json_encode( getExperimentStatus( $gfacid ), JSON_PRETTY_PRINT ) . "\n";
-echoline();
+gfacstatus( $gfacid );
 
 if ( $cancel && cancelAiravataJob( $gfacid ) ) {
     echo "canceled $gfacid\n";
