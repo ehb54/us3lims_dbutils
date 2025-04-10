1c385ff636530dba239829ab035c29912056231b
ultrascan3 system user <us3@nrch.umt.edu>
2025-04-09 21:39:18 -0700

    uslims_git_info.php - add directory ignore section

--- uslims_git_info.php
+++ uslims_git_info.php
@@ -22,6 +22,9 @@ $reposearchpaths =
      ,$wwwpath
     ];
 
+## regexp list of directory names to ignore
+$ignore_repos = [ '/undrop-for-innodb/' ];
+
 # user defines continued
 $known_repos =
     [
@@ -407,6 +410,19 @@ $repodirs = [];
 foreach ( $reposearchpaths as $v ) {
     $repodirs = array_merge( $repodirs, array_filter( explode( "\n", trim( run_cmd( "find $v -follow -name '*.git' 2>/dev/null | sed 's/.git\$//' | sed 's/\/\$//'" ) ) ) ) );
 }
+
+debug_json( "repodirs before ignore", $repodirs );
+
+foreach ( $repodirs as $k => $v ) {
+    foreach ( $ignore_repos as $ignore ) {
+        if ( preg_match( $ignore, $v ) ) {
+            unset( $repodirs[ $k ] );
+        }
+    }
+}
+            
+debug_json( "repodirs after ignore", $repodirs );
+error_exit( "testing" );
 
 $repos = (object)[];
 
